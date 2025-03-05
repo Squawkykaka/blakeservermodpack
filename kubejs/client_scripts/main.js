e6bd536457431435db67ce545b54d33f840b0cc8
George <squawkykaka@gmail.com>
2025-03-05 15:12:25 +1300

    Add more stuff

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -39,9 +39,36 @@ RecipeViewerEvents.groupEntries("item", event => {
     event.group('/oxidized_/', 'kubejs:oxidized_copper', 'Oxidized Copper')
     event.group('/_pattern/', 'kubejs:banner_pattern', 'Banner Patterns')
 
-
-    const useNbt = ['painting', 'goat_horn', "minecraft:suspicious_stew"]
     event.group("goat_horn", "kubejs:goat_horns", "Goat Horns")
     event.group("minecraft:suspicious_stew", "kubejs:suspicious_stews", "Suspicious Stews")
     event.group("minecraft:painting", "kubejs:paintings", "Paintings")
+    
+    event.group("ominous_bottle", "kubejs:ominous_bottle", "Ominous Bottles")
+    event.group('#minecraft:rails', "kubejs:rails", "Rails")
+
+    event.group('#minecraft:walls', "kubejs:walls", "Walls")
+    event.group('#minecraft:doors', "kubejs:doors", "Doors")
+    event.group('#minecraft:trapdoors', "kubejs:trapdoors", "Trapdoors")
+    event.group('#c:fence_gates', "kubejs:fence_gates", "Fence Gates")
+    event.group('#minecraft:slabs', "kubejs:slabs", "Slabs")
+    event.group('#minecraft:stairs', "kubejs:stairs", "Stairs")
+    event.group('#minecraft:fences', "kubejs:fences", "Fences")
+    event.group('#minecraft:buttons', "kubejs:buttons", "Buttons")
+    // Fix, as this is a block tag
+    event.group('#minecraft:pressure_plates', "kubejs:pressure_plates", "Pressure Plates")
+    event.group('#minecraft:planks', "kubejs:planks", "Planks")
+    event.group('#c:stripped_logs', "kubejs:stripped_logs", "Stripped Logs")
+    event.group('#minecraft:logs', 'kubejs:logs', 'Logs')
+
+
+
+
+
+
+    event.group(['minecraft:cut_copper', 'minecraft:copper_block', 'minecraft:chiseled_copper', 'minecraft:cut_copper_stairs', 'minecraft:cut_copper_slab', 'minecraft:waxed_copper_block', 'minecraft:waxed_chiseled_copper', 'minecraft:waxed_cut_copper', 'minecraft:waxed_cut_copper_stairs', 'minecraft:waxed_cut_copper_slab', 'minecraft:waxed_copper_grate', 'minecraft:copper_grate', 'minecraft:waxed_copper_trapdoor', 'minecraft:copper_trapdoor', 'minecraft:waxed_copper_door', 'minecraft:copper_door', 'minecraft:copper_bulb', 'minecraft:waxed_copper_bulb'], "kubejs:copper", "Copper")
+})
+
+RecipeViewerEvents.removeEntries("item", event => {
+    // Remove creative items
+    event.remove(["light", "barrier", "command_block", "command_block_minecart", "chain_command_block", "repeating_command_block", 'minecraft:structure_void', 'minecraft:structure_block', 'minecraft:jigsaw', 'minecraft:written_book', 'minecraft:petrified_oak_slab'])
 })
