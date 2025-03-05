16e7285fb9e0b587127842c6281bfe17d4645e19
George <squawkykaka@gmail.com>
2025-03-05 14:05:37 +1300

    Add distant horizens and start gorupng stuff

--- kubejs/client_scripts/main.js
+++ kubejs/client_scripts/main.js
@@ -1,9 +1,7 @@
-// Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded client example script)')
 
 RecipeViewerEvents.groupEntries("item", event => {
     event.group("#minecraft:decorated_pot_sherds", 'kubejs:pottery_sherd', "Pottery Sherd")
-
     event.group("#minecraft:chest_armor", 'kubejs:chestplates', "Chestplates")
     event.group("#minecraft:foot_armor", 'kubejs:boots', "Boots")
     event.group("#minecraft:head_armor", 'kubejs:helmets', "Helmets")
@@ -20,4 +18,30 @@ RecipeViewerEvents.groupEntries("item", event => {
     event.group('#comforts:sleeping_bags', "kubejs:sleeping_bags", "Sleeping Bags")
     event.group('#comforts:hammocks', "kubejs:hammocks", "Hammocks")
 
+    event.group('#minecraft:terracotta', "kubejs:terracotta", "Terracotta")
+    event.group('#minecraft:wool', 'kubejs:wool', 'Wool')
+    event.group('#minecraft:wool_carpets', 'kubejs:wool_carpets', 'Carpets')
+    event.group('#c:glass_blocks', 'kubejs:glass_blocks', 'Glass Blocks')
+    event.group('#c:glass_panes', 'kubejs:glass_panes', 'Glass Panes')
+    event.group('#c:glazed_terracottas', 'kubejs:glazed_terracotta', 'Glazed Terracotta')
+    event.group('#c:concretes', 'kubejs:concrete', 'Concrete')
+    event.group('#c:concrete_powders', 'kubejs:concrete_powder', 'Concrete Powder')
+    event.group('#minecraft:beds', 'kubejs:beds', 'Beds')
+    event.group('#minecraft:banners', 'kubejs:banners', 'Banners')
+    event.group('#minecraft:candles', 'kubejs:candles', 'Candles')
+    event.group('#c:dyes', 'kubejs:dyes', 'Dyes')
+
+    // This should show if the group is being correctly applied or not
+    event.group("#c:shulker_boxes", 'kubejs:shulker_boxes', 'Shulker Boxes')
+
+    event.group('/exposed_/', 'kubejs:exposed_copper', 'Exposed Copper')
+    event.group('/weathered_/', 'kubejs:weathered_copper', 'Weathered Copper')
+    event.group('/oxidized_/', 'kubejs:oxidized_copper', 'Oxidized Copper')
+    event.group('/_pattern/', 'kubejs:banner_pattern', 'Banner Patterns')
+
+
+    const useNbt = ['painting', 'goat_horn', "minecraft:suspicious_stew"]
+    event.group("goat_horn", "kubejs:goat_horns", "Goat Horns")
+    event.group("minecraft:suspicious_stew", "kubejs:suspicious_stews", "Suspicious Stews")
+    event.group("minecraft:painting", "kubejs:paintings", "Paintings")
 })
