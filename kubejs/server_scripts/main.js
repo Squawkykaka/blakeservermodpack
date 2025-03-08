f744d017cd94b33cbad7053708b578e6393202a1
George <noreply@example.com>
2025-03-08 17:50:49 +1300

    Change stuff

--- kubejs/server_scripts/main.js
+++ kubejs/server_scripts/main.js
@@ -1,3 +1,18 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded server example script)')
 
+ServerEvents.recipes(event => {
+    event.remove({id: 'waystones:warp_stone'})
+    event.recipes.minecraft.crafting_shaped('waystones:warp_stone', [
+            ' E ',
+            'ESE',
+            ' E '
+        ], {
+        E: 'minecraft:copper_ingot',
+        S: "minecraft:redstone"
+    })
+
+    event.forEachRecipe({input: 'waystones:warp_stone'}, recipe => {
+        recipe.replaceIngredient("minecraft:obsidian", "minecraft:stone_bricks")
+    })
+})
